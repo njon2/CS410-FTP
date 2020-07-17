63ad23828ddecf330d6f8e84e94aa8290b2cf383
Jon Niedermeyer <niedermeyerjf@gmail.com>
2020-07-16 18:10:56 -0700

    Initial commit with example code from Apache.

---
+++ src/com/company/Client.java
@@ -0,0 +1,12 @@
+//import
+
+public class Client {
+
+    private int port;
+    private String server_address;
+    private String user;
+    private String password;
+    private Client client;
+
+    public void 
+}
