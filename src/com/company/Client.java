0e07a87b3fd5ea75a06e22dd6a581025ab1ffb23
Brandon <brandonaltermatt@gmail.com>
2020-07-22 22:58:37 -0700

    Added test cases to Client.startConnection method

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,9 +1,13 @@
+/*
+This is the client class.
+Its purpose is to hold the client information
+and interact with a server.
+ */
+
 package com.company;
 
-//import
-
 import org.apache.commons.net.ftp.FTPClient;
-
+import org.apache.commons.net.ftp.FTPReply;
 import java.io.IOException;
 
 public class Client {
@@ -14,6 +18,7 @@ public class Client {
     private String pass;
     private FTPClient apacheFTPClient;
 
+    /* Connect to a server or throw an exception if the connection fails */
     public void startConnection(String hostname, String username, String password) throws IOException {
         apacheFTPClient = new FTPClient();
         port = 22;
@@ -22,5 +27,12 @@ public class Client {
         pass = password;
 
         apacheFTPClient.connect(hostname, port);
+        apacheFTPClient.login(username, password);
+
+        int reply = apacheFTPClient.getReplyCode();
+        if(!FTPReply.isPositiveCompletion(reply)){
+            apacheFTPClient.disconnect();
+            throw new IOException("Negative reply from FTP server, aborting, id was " + reply);
+        }
     }
 }
