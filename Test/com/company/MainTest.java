6233cf707d5893067f153c2975ffba5159e8e3e9
Brandon <brandonaltermatt@gmail.com>
2020-07-22 20:54:17 -0700

    Added test cases to Client.startConnection method

--- Test/com/company/MainTest.java
+++ Test/com/company/MainTest.java
@@ -2,20 +2,38 @@ package com.company;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import java.io.IOException;
 
-/*We will create a test file like this for every class that we write. The imports above import Junit 4 and 5
-  then each class/method can be tested.
-  The @Test keyword is used to signify Tests, and it basically tells if the
- */
+/*
+We will create a test file like this for every class that we write
+The imports above import Junit 4 and 5
+The @Test keyword is used to signify Tests
+*/
 
 public class MainTest {
 
-    @Test //The @Test is placed before all Tests, to signify a Test
-    public void StartConnection()
-    {
-        Assertions.assertEquals(1, 1); //Asserts that the first arg (Expected)
-                                                        // is the same as the second(actual)
-        Assertions.assertEquals("hello", "world"); //This will fail, and output data
-                                                                    //About the number of passed vs failed tests
+    /* Assert client can connect to a test server */
+    @Test
+    public void testStartConnection() {
+        boolean isConnected = true;
+        Client ftpClient = new Client();
+
+        try {
+            ftpClient.startConnection("ftp://speedtest.tele2.net", "anonymous", "anonymous");
+        } catch (IOException e) {
+            System.out.print(e.getMessage());
+            isConnected = false;
+        }
+
+        Assertions.assertTrue(isConnected);
+    }
+
+    /* Assert passing null parameters throws an exception */
+    @Test
+    public void testNullStartConnection() {
+        Client ftpClient = new Client();
+
+        Assertions.assertThrows(IOException.class, () ->
+            ftpClient.startConnection("", "", ""));
     }
 }
