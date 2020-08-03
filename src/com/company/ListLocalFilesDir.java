1e9e9e3ab07f83a99204957b94d548b078ded9f8
Omar Scott <vscott@pdx.edu>
2020-08-02 21:28:44 -0700

    Check for local dir

--- src/com/company/ListLocalFilesDir.java
+++ src/com/company/ListLocalFilesDir.java
@@ -6,4 +6,13 @@ import java.io.File;
 import java.io.IOException;
 
 public class ListLocalFilesDir {
+    public static void main(String[] args) {
+        File dir;
+        dir = new File("test");
+        File[] list = dir.listFiles();
+
+        if (list == null) {
+            System.out.println("Directory does not exist");
+        }
+    }
 }
