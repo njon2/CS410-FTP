0eeabe8e0a49c11dd6ff9404e3126be00c8dffe9
Brandon <brandonaltermatt@gmail.com>
2020-08-06 18:30:08 -0700

    Fixed get file for the general case

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -7,20 +7,20 @@ and interact with a server.
 package com.company;
 
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPSClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 import org.apache.commons.net.ftp.FTP;
-
 import java.io.*;
-import java.util.Collection;
 
 public class Client {
     private boolean loggedIn;
     private int port;
     private String server_address;
-    private String user;
-    private String pass;
     private FTPClient apacheFTPClient;
+
+    public Client() throws IOException {
+    }
 
     /* Connect to a server or throw an exception if the connection fails */
     public void startConnection(String hostname) throws IOException {
@@ -45,23 +45,17 @@ public class Client {
 
     /* Print the name of every file in the directory */
     public void list() throws IOException {
-        System.out.println("Reading file names: ");
         FTPFile[] files = apacheFTPClient.listFiles();
-        System.out.println("Starting printing file names: ");
         for (FTPFile file : files) {
-            System.out.println("File :" + file.getName());
+            System.out.println(file.getName());
         }
     }
 
     public boolean logoff() {
         if (loggedIn) {
             try {
-                //return apacheFTPClient.logout();
                 apacheFTPClient.logout();
-                //apacheFTPClient.disconnect();
-                System.out.println("Logout successful.");
             } catch (IOException e) {
-                System.out.println("Something went wrong.");
                 e.printStackTrace();
             }
             loggedIn = false;
@@ -70,24 +64,19 @@ public class Client {
         return false;
     }
 
-    public void enterLocalPassiveMode() {
-        apacheFTPClient.enterLocalPassiveMode();
-    }
-
     /*retrieves a file from the remote server and downloads to C:/Users/Default/Downloads*/
     public boolean get(String filePath) throws IOException {
 
-        //apacheFTPClient.enterLocalPassiveMode();
+        apacheFTPClient.enterLocalPassiveMode();
         apacheFTPClient.setFileType(FTP.BINARY_FILE_TYPE);
 
-        //File downloadFile = new File("C/Users/Omar/Desktop/Testing" + filePath);
-        File downloadFile = new File("/Users/Omar/Desktop/Testing/" + filePath);
+        File downloadFile = new File("C:/Users/Default/Downloads/" + filePath);
         OutputStream outStream = new BufferedOutputStream(new FileOutputStream(downloadFile));
         boolean success = apacheFTPClient.retrieveFile('/' + filePath, outStream);
 
         outStream.close();
 
-        if (success) {
+        if(success) {
             System.out.println(filePath + " has been downloaded\n");
             return true;
         }
@@ -96,37 +85,5 @@ public class Client {
         return false;
 
     }
-
-    public File[] readFiles(String directory) {
-        File dir = new File(directory);
-        File[] list = dir.listFiles();
-        if (list == null) {
-            System.out.println("Directory does not exist");
-        } else {
-            System.out.println(dir);
-            for (File file : list) {
-                if (file.isFile())
-                    System.out.println("File is " + file.getName());
-                else if (file.isDirectory())
-                    System.out.println("Directory is " + file.getName());
-            }
-        }
-        return list;
-    }
-
-    public boolean isFilePresent(String directory, String toGet) {
-        File dir = new File(directory);
-        File[] list = dir.listFiles();
-        if (list == null) {
-            System.out.println("Directory does not exist");
-            return false;
-        }
-        for (File file : list) {
-            if (file.getName().equals(toGet)) {
-                System.out.println(true);
-                return true;
-            }
-        }
-        return false;
-    }
 }
+
